afdf551fc2bc25d9eee7433d741372aa1349b496
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-06-02 21:58:11 +0200

    folder image profilo + user + err hand + error

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
     path: "backend/config/.env",
   });
+  app.use(bodyParser.urlencoded({ extended: true }));
 }
+
+//import routes
+
+const user = require("./controller/user");
+app.use("/api/v2/user", user);
+
+//errorHandler
+
+app.use(ErrorHandler);
 
 module.exports = app;
