9bae6833c44f775238cc3213e91b7f388a10e7d5
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-06-04 23:59:21 +0200

    homepage semi finish

--- frontend/src/components/Layout/Home.jsx
+++ frontend/src/components/Layout/Home.jsx
@@ -9,7 +9,20 @@ import feat3 from "../../styles/img/features/f4.png";
 import feat4 from "../../styles/img/features/f5.png";
 import feat5 from "../../styles/img/features/f6.png";
 import pro1 from "../../styles/img/products/f1.jpg";
-
+import pro2 from "../../styles/img/products/f2.jpg";
+import pro3 from "../../styles/img/products/f3.jpg";
+import pro4 from "../../styles/img/products/f4.jpg";
+import pro5 from "../../styles/img/products/f5.jpg";
+import pro6 from "../../styles/img/products/f6.jpg";
+import pro7 from "../../styles/img/products/f7.jpg";
+import pro8 from "../../styles/img/products/n1.jpg";
+import pro9 from "../../styles/img/products/n2.jpg";
+import pro10 from "../../styles/img/products/n3.jpg";
+import pro11 from "../../styles/img/products/n4.jpg";
+import pro12 from "../../styles/img/products/n5.jpg";
+import pro13 from "../../styles/img/products/n6.jpg";
+import pro14 from "../../styles/img/products/n7.jpg";
+import pro15 from "../../styles/img/products/n8.jpg";
 const Home = () => {
   return (
     <div>
@@ -108,7 +121,7 @@ const Home = () => {
             </div>
           </div>
           <div className="pro">
-            <img src={pro1} alt="" />
+            <img src={pro2} alt="" />
             <div className="des">
               <span>Adidas</span>
               <h5>Astronaut T-shirt</h5>
@@ -138,7 +151,7 @@ const Home = () => {
             </div>
           </div>
           <div className="pro">
-            <img src={pro1} alt="" />
+            <img src={pro3} alt="" />
             <div className="des">
               <span>Adidas</span>
               <h5>Astronaut T-shirt</h5>
@@ -168,7 +181,7 @@ const Home = () => {
             </div>
           </div>
           <div className="pro">
-            <img src={pro1} alt="" />
+            <img src={pro4} alt="" />
             <div className="des">
               <span>Adidas</span>
               <h5>Astronaut T-shirt</h5>
@@ -198,7 +211,7 @@ const Home = () => {
             </div>
           </div>
           <div className="pro">
-            <img src={pro1} alt="" />
+            <img src={pro5} alt="" />
             <div className="des">
               <span>Adidas</span>
               <h5>Astronaut T-shirt</h5>
@@ -228,7 +241,7 @@ const Home = () => {
             </div>
           </div>
           <div className="pro">
-            <img src={pro1} alt="" />
+            <img src={pro6} alt="" />
             <div className="des">
               <span>Adidas</span>
               <h5>Astronaut T-shirt</h5>
@@ -258,7 +271,7 @@ const Home = () => {
             </div>
           </div>
           <div className="pro">
-            <img src={pro1} alt="" />
+            <img src={pro7} alt="" />
             <div className="des">
               <span>Adidas</span>
               <h5>Astronaut T-shirt</h5>
@@ -288,7 +301,7 @@ const Home = () => {
             </div>
           </div>
           <div className="pro">
-            <img src={pro1} alt="" />
+            <img src={pro8} alt="" />
             <div className="des">
               <span>Adidas</span>
               <h5>Astronaut T-shirt</h5>
@@ -317,6 +330,284 @@ const Home = () => {
               </a>
             </div>
           </div>
+        </div>
+      </section>
+
+      <section id="banner" className="section-m1">
+        <h4>Repair Service</h4>
+        <h2>
+          Up to <span>70% Off</span> - All t-Shirt & Accesories
+        </h2>
+        <button className="normal">Explore More</button>
+      </section>
+
+      <section id="product1" className="section-p1">
+        <h2>New Arrivals</h2>
+        <p>Summer Collection New Modern Design</p>
+        <div className="pro-container">
+          <div className="pro">
+            <img src={pro8} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+          <div className="pro">
+            <img src={pro9} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+          <div className="pro">
+            <img src={pro10} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+          <div className="pro">
+            <img src={pro11} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+          <div className="pro">
+            <img src={pro12} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+          <div className="pro">
+            <img src={pro13} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+          <div className="pro">
+            <img src={pro14} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+          <div className="pro">
+            <img src={pro15} alt="" />
+            <div className="des">
+              <span>Adidas</span>
+              <h5>Astronaut T-shirt</h5>
+              <div className="cart">
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+                <i>
+                  <RxStar />
+                </i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <div className="cartLogo">
+              <a href="#">
+                <FaShoppingCart />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="sm-banner" className="section-p1">
+        <div className="banner-box">
+          <h4>Crazy deals</h4>
+          <h2>buy 1 get 1 free</h2>
+          <span>The best Summer dress</span>
+          <button className="white">Learn More</button>
+        </div>
+
+        <div className="banner-box2">
+          <h4>Spring/Summer</h4>
+          <h2>Upcoming Season</h2>
+          <span>The best classic dress</span>
+          <button className="white">Learn More</button>
+        </div>
+      </section>
+
+      <section>
+        <div className="banner-box3">
+          <h2>SEASONALE SALE</h2>
+          <h3>Winter Collection -50% OFF</h3>
         </div>
       </section>
     </div>
